8d656aac531c0387f094e411aa8edf3aab0e8cea
Alfredo Gimenez <gimenez1@llnl.gov>
2016-02-10 15:59:17 -0800

    Workon scripts for Caliper

--- home/.bash.d/workon.sh
+++ home/.bash.d/workon.sh
@@ -21,7 +21,43 @@ function end_section {
     CURRENT_SECTION="INVALID"
 }
 
+# command-exists ===========================================
+
+function command-exists {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# use/load abstraction ======================================
+
+function choose {
+    if command-exists use; then
+        use $@
+    elif command-exists module; then
+        module load $@
+    else
+        echo "No way to choose!"
+        return 1
+    fi
+}
+
+function spoose {
+    if command-exists use; then
+        spack use $@
+    elif command-exists module; then
+        spack load $@
+    else
+        echo "No way to spoose!"
+        return 1
+    fi
+}
+
 # setup ====================================================
+
+function setup-general {
+    spoose git
+    spoose vim
+    spoose cmake
+}
 
 function setup-spark {
     begin_section "Spark setup"
@@ -40,29 +76,57 @@ function setup-bigfoot {
     end_section
 }
 
-function setup-scrubjay-virtualenv {
+function setup-scrubjay-env {
     begin_section "ScrubJay development environment setup"
     export PATH=$HOME/anaconda2/bin:$PATH
     source activate scrubjay
     end_section
 }
 
+function setup-caliper-env {
+    begin_section "Caliper development environment setup"
+    if command-exists use; then
+        use gcc-4.9.2p
+    elif command-exists module; then
+        module load gnu/4.9.2
+    fi
+    end_section
+}
+
 # workon ====================================================
 
+function hostname-stripped {
+    ${HOSTNAME//[[:digit:]]/}
+}
+
 function workon-scrubjay {
-    setup-bigfoot
-    setup-scrubjay-virtualenv
+    setup-scrubjay-env
     setup-spark
 }
 
+function workon-caliper {
+    setup-caliper-env
+}
+
 function workon {
-    if [ "$#" -ne 1 ]; then
-        echo "Usage: workon <project>"
-        return 1
+    # Machine-specific setup
+    MACHINE=${HOSTNAME//[[:digit:]]/}
+    if [ "$MACHINE" == "cab" ]; then
+        setup-general
+    elif [ "$MACHINE" == "bigfoot" ]; then
+        setup-bigfoot
     fi
 
+    # Check if a project was specified
+    if [ "$#" -ne 1 ]; then
+        return 0
+    fi
+
+    # Project-specific setup
     if [ "$1" == "scrubjay" ]; then
         workon-scrubjay
+    elif [ "$1" == "caliper" ]; then
+        workon-caliper
     else
         echo "Project $1 not found!"
         return 1
